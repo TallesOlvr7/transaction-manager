95b64c0c726edcbebb635026e3152bb8ba8fb76d
TallesOlvr7 <tallesgabrieldeoliveirarodrig@gmail.com>
2024-09-29 01:30:26 -0300

    feat:first commit

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class User extends Authenticatable
+{
+    use HasFactory,HasUuids;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
---
+++ database/migrations/2024_09_29_041409_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('payer');
+            $table->uuid('payee');
+
+            $table->foreign('payer')
+                  ->references('id')
+                  ->on('users');
+            $table->foreign('payee')
+                  ->references('id')
+                  ->on('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
